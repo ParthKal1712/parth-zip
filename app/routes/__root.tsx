4fdc1502ec5dfae6337035d50895f3a522cc92fd
Parth Kalra <parth.kalra1712@gmail.com>
2025-03-16 21:00:03 -0600

    🖼️ Added Site Title and Favicon.

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -19,7 +19,13 @@ export const Route = createRootRoute({
         content: "width=device-width, initial-scale=1",
       },
       {
-        title: "TanStack Start Starter",
+        title: "Parth Kalra",
+      },
+    ],
+    links: [
+      {
+        rel: "icon",
+        href: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>⚡</text></svg>",
       },
     ],
   }),
